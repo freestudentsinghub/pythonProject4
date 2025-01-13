5672399d454a6063632db33be72bd91d1754441a
freestudentsinghub <masha1207355@gmail.com>
2025-01-13 16:36:47 +0300

    add form product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import HomeView, ContactsView, ProductDetailView, ProductList
+from catalog.views import HomeView, ContactsView, ProductDetailView, ProductList, ProductCreateView, ProductUpdateView
 
 app_name = CatalogConfig.name
 
@@ -8,7 +8,9 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='product_ditail'),
-    path('home', ProductList.as_view(), name='product_list')
+    path('home', ProductList.as_view(), name='product_list'),
+    path('product/create', ProductCreateView.as_view(), name='product_create'),
+    path('product/update/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
 ]
 
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,48 +1,35 @@
 
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView
 
+from catalog.forms import ProductForm
 from catalog.models import Product
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
 
 class HomeView(TemplateView):
     template_name = "catalog/home.html"
 
-# def home(request):
-#     return render(request, 'home.html')
 
 class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
-
-# def contacts(request):
-#     return render(request, 'contacts.html')
 
 
 class ProductDetailView(DetailView):
     model = Product
 
-# def product_ditail(request, pk):
-#     product = Product.objects.get(pk=pk)
-#     context = {
-#         "product_name": product.name,
-#         "product_description": product.description,
-#         "product_image": product.image,
-#         "product_category": product.category,
-#         "product_cost": product.cost,
-#         "product_created_at": product.created_at,
-#         "product_updated_at": product.updated_at,
-#                }
-#     return render(request, 'product_detail.html', context)
-
-# catalog/product_list.html detail
-
 
 class ProductList(ListView):
     model = Product
 
-
-# def product_list(request):
-#     products = Product.objects.all()
-#     context = {
-#         "products": products,
-#     }
-#     return render(request, 'home.html', context=context)
