e7ebd4e44943ee295c7a9f32edae6f88478ce9c1
freestudentsinghub <masha1207355@gmail.com>
2025-01-21 16:52:23 +0300

    add 2 two work with admin

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseForbidden
 from django.shortcuts import render, get_object_or_404, redirect
@@ -9,13 +9,23 @@ from django.views.generic import ListView, DetailView, TemplateView, CreateView,
 from catalog.forms import ProductForm, ProductModeratorForm
 from catalog.models import Product
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:product_list')
 
-class ProductUpdateView(LoginRequiredMixin, UpdateView):
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial["owner"] = self.request.user
+        return initial
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
@@ -28,6 +38,14 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
         if user.has_perm("product.can_unpublish_product"):
             return ProductModeratorForm
         raise PermissionDenied
+
+    def test_func(self):
+        return self.request.user == self.get_object().owner
+
+    def handle_no_permission(self):
+        if self.raise_exception:
+            raise PermissionDenied
+        return redirect('catalog:product_list')
 
 class HomeView(TemplateView):
     template_name = "catalog/home.html"
@@ -45,7 +63,7 @@ class ProductList(ListView):
     model = Product
 
 
-class ProductModeratorsView(LoginRequiredMixin, View):
+class ProductModeratorsView(LoginRequiredMixin, UserPassesTestMixin, View):
     def post(self, request, product_id):
         product = get_object_or_404(Product, id=product_id)
 
@@ -54,5 +72,13 @@ class ProductModeratorsView(LoginRequiredMixin, View):
 
         product.delete()
 
+        return redirect('catalog:product_list')
+
+    def test_func(self):
+        return self.request.user == self.get_object().owner
+
+    def handle_no_permission(self):
+        if self.raise_exception:
+            raise PermissionDenied
         return redirect('catalog:product_list')
 
