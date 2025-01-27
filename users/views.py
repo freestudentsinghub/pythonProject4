e9768264411c50dc3f83f84cbc74c00efca18686
freestudentsinghub <masha1207355@gmail.com>
2025-01-27 17:47:38 +0300

    add 27 homework 2 work

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm
+
+class RegisterView(CreateView):
+    template_name = 'users/register.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('catalog:product_list')
